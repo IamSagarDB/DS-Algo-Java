2ace8cf59421dc909af3399201c149ea895ef3c2
DropCodes <sagardb50@gmail.com>
2020-01-08 22:16:23 +0530

    added Queue Class

--- src/DS/Queue/Queue.java
+++ src/DS/Queue/Queue.java
@@ -1,5 +1,52 @@
 package DS.Queue;
 
 public class Queue {
-
+	int[] queue;
+	int front = 0;
+	int rear = 0;
+	int size;
+	
+	// queue Constructor
+	public Queue(int size){
+		this.size = size;
+		queue = new int[size];
+		rear = 0;
+		front = 0;
+	}
+	
+	//If queue is not full then this function adds the element to back of the queue or else it will print 'OverFlow' 
+	public void enqueue(int element) {
+		if(rear==size) {
+			System.out.print("Overflow");
+		}else {
+			queue[rear]=element;
+			rear++;
+		}
+	}
+	//If queue is not empty then this function removes the element from front of the queue or else it will print 'UnderFlow'
+	public void dequeue() {
+		if(front == rear) {
+			System.out.print("Underflow");
+		}else {
+			queue[front]=0;
+			front++;
+		}
+	}
+	// This function return front element of queue
+	public int front() {
+		return queue[front];
+		
+	}
+	
+	// this function check if the queue isEmpty
+	public boolean isEmpty() {
+		return (front==rear);
+	}
+	// this function print element in queue
+	public void print() {
+		for(int x : queue) {
+			System.out.println(x);
+		}
+	}
+	
 }
--- src/DS/Queue/QueueTest.java
+++ src/DS/Queue/QueueTest.java
@@ -3,8 +3,14 @@ package DS.Queue;
 public class QueueTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Queue queue =new Queue(3);
+		queue.enqueue(10);
+		queue.enqueue(20);
+		queue.enqueue(30);
+		queue.print();
+		queue.dequeue();
+		queue.print();
+		System.out.println(queue.front());;
 	}
 
 }
