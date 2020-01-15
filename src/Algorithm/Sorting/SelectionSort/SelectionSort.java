c18d296f0031cda0cd5aae3859a90c5fef8153ca
DropCodes <sagardb50@gmail.com>
2020-01-15 13:53:05 +0530

    updated BubbleSort and SelectionSort

--- src/Algorithm/Sorting/SelectionSort/SelectionSort.java
+++ src/Algorithm/Sorting/SelectionSort/SelectionSort.java
@@ -2,28 +2,28 @@ package Algorithm.Sorting.SelectionSort;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] arr = {2, 133, 88, 11, 6, 5, 3, 7, 9, 1,0};
-        int[] res = SelectionSort(arr);
-        for (int x : res) {
+        int[] arr = {4, 7,200, 2, 9, 5, 6,100,0};
+        SelectionSort(arr);
+        for (int x : arr) {
             System.out.print(x + " ");
         }
-
     }
 
     // Algorithm for SelectionSort
-    public static int[] SelectionSort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
+    public static void SelectionSort(int[] arr) {
+        for (int i = 0; i < arr.length ; i++) {
+
             int minIndex = i;
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < array[minIndex]) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
-                int temp = array[minIndex];
-                array[minIndex] = array[i];
-                array[i] = temp;
             }
-        }
 
-        return array;
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
     }
 }
