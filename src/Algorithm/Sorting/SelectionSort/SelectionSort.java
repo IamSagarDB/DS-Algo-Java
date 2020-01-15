0c8610ecc8d4f0c354a2faeb98dd8493d3b5c9b8
DropCodes <sagardb50@gmail.com>
2020-01-15 12:04:53 +0530

    fixed mistakes

--- src/Algorithm/Sorting/SelectionSort/SelectionSort.java
+++ src/Algorithm/Sorting/SelectionSort/SelectionSort.java
@@ -3,15 +3,15 @@ package Algorithm.Sorting.SelectionSort;
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {2, 133, 88, 11, 6, 5, 3, 7, 9, 1,0};
-        int[] res = InsertionSort(arr);
+        int[] res = SelectionSort(arr);
         for (int x : res) {
             System.out.print(x + " ");
         }
 
     }
 
-    // Algorithm for insertion sort
-    public static int[] InsertionSort(int[] array) {
+    // Algorithm for SelectionSort
+    public static int[] SelectionSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j < array.length; j++) {
