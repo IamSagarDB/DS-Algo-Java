1854f040ecc27a23c455ddb52b0ba43eebeef2af
DropCodes <sagardb50@gmail.com>
2020-01-15 12:03:05 +0530

    Create SelectionSort.java

---
+++ src/Algorithm/Sorting/SelectionSort/SelectionSort.java
@@ -0,0 +1,29 @@
+package Algorithm.Sorting.SelectionSort;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {2, 133, 88, 11, 6, 5, 3, 7, 9, 1,0};
+        int[] res = InsertionSort(arr);
+        for (int x : res) {
+            System.out.print(x + " ");
+        }
+
+    }
+
+    // Algorithm for insertion sort
+    public static int[] InsertionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+                int temp = array[minIndex];
+                array[minIndex] = array[i];
+                array[i] = temp;
+            }
+        }
+
+        return array;
+    }
+}
