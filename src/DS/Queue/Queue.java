215905d553392ff1a7bffec450b34a29541a8453
DropCodes <sagardb50@gmail.com>
2020-01-16 15:30:50 +0530

    Update Queue.java

--- src/DS/Queue/Queue.java
+++ src/DS/Queue/Queue.java
@@ -1,10 +1,10 @@
 package DS.Queue;
 
 public class Queue<T> {
+
     private class Node {
         private T data;
         private Node next;
-
         private Node(T data) {
             this.data = data;
         }
@@ -25,21 +25,19 @@ public class Queue<T> {
 
     public void enqueue(T data) {
         Node node = new Node(data);
-
-        if (head == null) {
+        if (head == null) {  // first it check if head is null then the first inserted node will point to head
             head = node;
         }
         if (tail != null) {
             tail.next = node;
         }
         tail_count++;
-        tail = node;
+        tail = node; // first ele inserted tail will point to first node and after that it will point to next node
     }
 
     public T dequeue() {
-
         T data = head.data;
-        head = head.next;
+        head = head.next;// head will point to next node address and when it reaches to end head will be assigned to null
         head_count++;
         return data;
     }
