495cfb0d5ceaabcd0a4d15a753e28870e75a6756
DropCodes <sagardb50@gmail.com>
2020-01-24 16:56:23 +0530

    Create CircularLinkedList.java

---
+++ src/DS/LinkedList/CircularLinkedList.java
@@ -0,0 +1,19 @@
+package DS.LinkedList;
+
+public class CircularLinkedList {
+    public class Node {
+        Node next;
+        int data;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public Node last;
+
+    public void insert(int data) {
+        
+    }
+}
+
